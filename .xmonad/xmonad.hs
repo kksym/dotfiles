70c7d42a3614d254f1b8087372ceb5442766fced
Kyle K <kyliek24@hotmail.co.uk>
2012-05-14 19:45:34 +0100

    tabs->spaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -40,39 +40,39 @@ myLayoutHook =
                             withIM 0.2 (Role "gimp-dock") (ResizableTall 3 (3 / 100) (1 / 2) [])			
 
 myPrettyPrinter xmobar = xmobarPP {
-        ppCurrent = xmobarColor "#0000ff" "#eeeeee" . pad,
-        ppHidden = xmobarColor "#00309f" "#fefefe",
-		ppOutput = hPutStrLn xmobar,
-		ppTitle = xmobarColor "#000000" "#ffffff" . (replicate 60 ' ' ++),
-        ppOrder = \(ws:layout:title:_) -> [ws],
-        ppSep = ""
+    ppCurrent = xmobarColor "#0000ff" "#eeeeee" . pad,
+    ppHidden = xmobarColor "#00309f" "#fefefe",
+    ppOutput = hPutStrLn xmobar,
+    ppTitle = xmobarColor "#000000" "#ffffff" . (replicate 60 ' ' ++),
+    ppOrder = \(ws:layout:title:_) -> [ws],
+    ppSep = ""
 }
 
 myDefaultConfig xmobar = defaultConfig {
-	terminal = "urxvt",
-	modMask = mod1Mask,
-	borderWidth = 2,
-	workspaces = myWorkspaces ++ map show [length myWorkspaces + 1..9],
-	normalBorderColor = "#eeeeee",
-	focusedBorderColor = "#cacaca",
-	startupHook = setWMName "LG3D", -- Make sure Java applications display
-	manageHook = manageDocks <+> myManageHook,
-	layoutHook = myLayoutHook,
-	logHook = dynamicLogWithPP $ myPrettyPrinter xmobar
+    terminal = "urxvt",
+    modMask = mod1Mask,
+    borderWidth = 2,
+    workspaces = myWorkspaces ++ map show [length myWorkspaces + 1..9],
+    normalBorderColor = "#eeeeee",
+    focusedBorderColor = "#cacaca",
+    startupHook = setWMName "LG3D", -- Make sure Java applications display
+    manageHook = manageDocks <+> myManageHook,
+    layoutHook = myLayoutHook,
+    logHook = dynamicLogWithPP $ myPrettyPrinter xmobar
 }
 
 myKeyBindings :: [((KeyMask, KeySym), X ())]
 myKeyBindings =
     [
-	((0, xK_Print), spawn "scrot -e 'mv $f ~/screen'"),
-	((mod1Mask .|. controlMask, xK_c), spawn "firefox"),
+    ((0, xK_Print), spawn "scrot -e 'mv $f ~/screen'"),
+    ((mod1Mask .|. controlMask, xK_c), spawn "firefox"),
     ((mod1Mask .|. controlMask, xK_u), spawn "uzbl-tabbed"),
-	((mod1Mask .|. controlMask, xK_p), spawn "pcmanfm"),
-	((mod1Mask .|. controlMask, xK_m), spawn "~/games/minecraft"),
-	((mod1Mask .|. controlMask, xK_e), spawn "eclipse"),	
-	((mod1Mask .|. controlMask, xK_g), spawn "gimp"),
-	((mod1Mask .|. controlMask, xK_n), spawn "nitrogen"),
-	((mod1Mask .|. controlMask, xK_d), spawn "desura"),
+    ((mod1Mask .|. controlMask, xK_p), spawn "pcmanfm"),
+    ((mod1Mask .|. controlMask, xK_m), spawn "~/games/minecraft"),
+    ((mod1Mask .|. controlMask, xK_e), spawn "eclipse"),	
+    ((mod1Mask .|. controlMask, xK_g), spawn "gimp"),
+    ((mod1Mask .|. controlMask, xK_n), spawn "nitrogen"),
+    ((mod1Mask .|. controlMask, xK_d), spawn "desura"),
     ((mod1Mask .|. controlMask, xK_t), spawn "thunderbird"),
     ((mod1Mask, xK_KP_Add), spawn "mpc volume +2"),
     ((mod1Mask, xK_KP_Subtract), spawn "mpc volume -2")
@@ -80,6 +80,6 @@ myKeyBindings =
 
 main :: IO ()
 main = do
-	xmobar <- spawnPipe "xmobar"
-	xmonad $ myDefaultConfig xmobar `additionalKeys` myKeyBindings
+    xmobar <- spawnPipe "xmobar"
+    xmonad $ myDefaultConfig xmobar `additionalKeys` myKeyBindings
 
